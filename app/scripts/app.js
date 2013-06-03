0d34e0308ae21e79ccb07d0a2d397e15b92eb1d7
Heinrich Kuhlmann <HeinrichKuhlmann@gmail.com>
2013-06-03 15:00:24 +0200

    init fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,8 +15,8 @@
 'use strict';
 
 var CONFIG = {
-  clientId: '502747173299.apps.googleusercontent.com',
-  apiKey: 'AIzaSyA8uaDCmQ1mvhjXQZvF55vW9ygO_fAYKRs',
+  clientId: '633280430175.apps.googleusercontent.com',
+  apiKey: 'AIzaSyCAaLh02Hg_iEzDytRxZPR3nsSWRVYriNQ',
   scopes: [
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/drive.install'
